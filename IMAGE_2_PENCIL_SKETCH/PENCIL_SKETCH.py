84d46e3038e98239dbc2c29bf99be72ae5a988d0
AyushSingh26 <61933844+AyushSingh26@users.noreply.github.com>
2021-09-26 17:15:29 +0530

    Add files via upload

---
+++ IMAGE_2_PENCIL_SKETCH/PENCIL_SKETCH.py
@@ -0,0 +1,22 @@
+import cv2
+
+image = cv2.imread("Lenna_(test_image).png")
+cv2.imshow("AYUSH", image)
+cv2.waitKey(0)
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#cv2.imshow("New Ayush", gray_image)
+cv2.waitKey(0)
+
+inverted_image = 255 - gray_image
+#cv2.imshow("Inverted", inverted_image)
+cv2.waitKey()
+
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+
+
+cv2.imshow("Sketch", pencil_sketch)
+cv2.waitKey(0)
